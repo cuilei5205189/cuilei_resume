66b7a35a2a316f4ceaa54a38205971cf634cd2fd
cuilei5205189 <allen5205189@gmail.com>
2018-07-27 09:34:10 +0800

    页面跟随nav动画

--- main.js
+++ main.js
@@ -11,10 +11,20 @@ portfolio3.onclick = function () {
 };
 
 //loading
+setTimeout(() => {
+    once()
+}, 3500)
+
 
 setTimeout(function () {
     loadingPage.classList.remove("active");
-}, 3000);
+}, 2000);
+
+
+let specialTags = document.querySelectorAll('[data-x]')
+for (let i = 0; i < specialTags.length; i++) {
+    specialTags[i].classList.add('offset')
+}
 
 window.onscroll = function (e) {
     if (window.scrollY > 0) {
@@ -24,7 +34,29 @@ window.onscroll = function (e) {
         topNavBarWrapper.classList.remove("sticky");
         topNavBar.classList.remove("sticky");
     }
-};
+    findClosest()
+}
+
+function findClosest() {
+    let specialTags = document.querySelectorAll('[data-x]')
+
+    let minIndex = 0
+    for (let i = 0; i < specialTags.length; i++) {
+        if (Math.abs(specialTags[i].offsetTop - window.scrollY) < Math.abs(specialTags[minIndex].offsetTop - window.scrollY)) {
+            minIndex = i
+        }
+    }
+    specialTags[minIndex].classList.remove('offset')
+    specialTags[minIndex].classList.add('active')
+    let id = specialTags[minIndex].id
+    let a = document.querySelector('a[href="#' + id + '"]')
+    let li = a.parentNode
+    let broAndme = li.parentNode.children
+    for (let i = 0; i < broAndme.length; i++) {
+        broAndme[i].classList.remove('highLight')
+    }
+    li.classList.add('highLight')
+}
 
 let liTags = document.querySelectorAll("nav.menu>ul>li");
 for (let i = 0; i < liTags.length; i++) {
@@ -39,6 +71,14 @@ for (let i = 0; i < liTags.length; i++) {
     }
 }
 
+
+// Setup the animation loop.
+function animate(time) {
+    requestAnimationFrame(animate);
+    TWEEN.update(time);
+}
+requestAnimationFrame(animate);
+
 let aTags = document.querySelectorAll('nav.menu>ul>li>a')
 for (let i = 0; i < aTags.length; i++) {
     aTags[i].onclick = function (e) {
@@ -48,6 +88,35 @@ for (let i = 0; i < aTags.length; i++) {
         let href = a.getAttribute('href')
         let element = document.querySelector(href)
         let top = element.offsetTop
-        window.scrollTo(0, top - 80)
+        let targetTop = top - 80
+        let currentTop = window.scrollY
+        let s = targetTop - currentTop
+        let t = Math.abs((s / 100) * 300)
+        if (t > 500) {
+            t = 500
+        }
+        // let n = 100 //一共动多少次
+        // let duration = 500 / n //多长时间动一次
+        // let distance = (targetTop - currentTop) / n
+        // let i = 0
+        // let id = setInterval(() => {
+        //     if (i === n) {
+        //         window.clearInterval(id)
+        //     }
+        //     i = i + 1
+        //     window.scrollTo(0, currentTop + distance * i)
+        // }, duration)
+        var coords = {
+            y: currentTop,
+        }
+        var tween = new TWEEN.Tween(coords)
+            .to({
+                y: targetTop
+            }, t)
+            .easing(TWEEN.Easing.Quadratic.InOut)
+            .onUpdate(function () {
+                window.scrollTo(0, coords.y)
+            })
+            .start();
     }
 }
