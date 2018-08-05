ccfe99a85329ae22d7582d48530d552930ea13f7
cuilei5205189 <allen5205189@gmail.com>
2018-08-05 11:51:28 +0800

    add swiper

--- main.js
+++ main.js
@@ -120,3 +120,26 @@ for (let i = 0; i < aTags.length; i++) {
             .start();
     }
 }
+
+
+var mySwiper = new Swiper('.swiper-container', {
+        // Optional parameters
+        direction: 'horizontal',
+        loop: true,
+
+        // If we need pagination
+        pagination: {
+            el: '.swiper-pagination',
+        },
+
+        // Navigation arrows
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+
+        // And if we need scrollbar
+        scrollbar: {
+            el: '.swiper-scrollbar',
+        },
+    }) 
